4e138bd8a30141c1eaf1000759e0329bbff3c4a2
yuchiu <yuchiu2002@hotmail.com>
2019-01-14 21:20:09 -0500

    setup create user mutation

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,16 +1,25 @@
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import { Field, ID, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class User extends BaseEntity {
+  @Field(() => ID)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   firstName: string;
 
+  @Field()
   @Column()
   lastName: string;
 
+  @Field()
+  name: string;
+
+  @Field()
   @Column("text", { unique: true })
   email: string;
 
