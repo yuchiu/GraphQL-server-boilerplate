f95d8148e9189c6c3df537bb66281ef1391fc3b6
yuchiu <yuchiu2002@hotmail.com>
2019-01-14 20:45:27 -0500

    initialize type-graphql server

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import * as Express from "express";
+import { buildSchema, Resolver, Query } from "type-graphql";
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello() {
+    return "hello world";
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver]
+  });
+  const apolloServer = new ApolloServer({ schema });
+  const app = Express();
+  apolloServer.applyMiddleware({ app });
+  app.listen(3030, () => {
+    console.log("server is listening on port: " + 3030 + "/graphql");
+  });
+};
+
+main();
