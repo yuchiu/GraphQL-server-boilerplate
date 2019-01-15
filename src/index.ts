7f1603ec7a71679bc4eb7b56bfca9ccc5702308c
yuchiu <yuchiu2002@hotmail.com>
2019-01-14 20:59:10 -0500

    setup database with typeorm

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import * as Express from "express";
 import { buildSchema, Resolver, Query } from "type-graphql";
+import { createConnection } from "typeorm";
 
 @Resolver()
 class HelloResolver {
@@ -12,12 +13,18 @@ class HelloResolver {
 }
 
 const main = async () => {
+  await createConnection();
+
   const schema = await buildSchema({
     resolvers: [HelloResolver]
   });
+
   const apolloServer = new ApolloServer({ schema });
+
   const app = Express();
+
   apolloServer.applyMiddleware({ app });
+
   app.listen(3030, () => {
     console.log("server is listening on port: " + 3030 + "/graphql");
   });
